f126c0c7da86c27564d021c8d78ad57baac41e1e
Germano06 <57481143+Germano06@users.noreply.github.com>
2022-09-23 11:20:32 +0530

    folder rename

---
+++ teams/Team-BASIC/Github_Tracker/github-tracker-nestjs/dist/git-users/users.entity.d.ts
@@ -0,0 +1,10 @@
+export declare class SUsers {
+    id: number;
+    email: string;
+    password: string;
+}
+export declare class Users {
+    id: number;
+    email: string;
+    username: string;
+}
