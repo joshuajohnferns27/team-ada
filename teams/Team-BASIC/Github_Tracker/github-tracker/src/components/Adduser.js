5f7f82e4184aeeff39cb508f2161a326b3a5cbc3
Aliyaj Ahemad <aliyajahemad@gmail.com>
2022-09-26 12:08:36 +0530

    fix : Removed body styling from Adduser.css
    body styling was affecting the login pages.

--- teams/Team-BASIC/Github_Tracker/github-tracker/src/components/Adduser.js
+++ teams/Team-BASIC/Github_Tracker/github-tracker/src/components/Adduser.js
@@ -4,34 +4,36 @@ import "./Adduser.css";
 export default function Adduser() {
   return (
     <>
-      <div className="login-box">
-        <h2>Add User</h2>
-        <form>
-          <div className="user-box">
-            <input type="text" name="" required="" />
-            <label>UserName</label>
-          </div>
-          <a href="#">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            Add
-          </a>
-          <a href="#">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            Upload CSV
-            <input
-              type="file"
-              className="inputFile"
-              accept=".csv"
-              style={{ pointer: "cursor" }}
-            ></input>
-          </a>
-        </form>
+      <div className="container">
+        <div className="login-box">
+          <h2>Add User</h2>
+          <form>
+            <div className="user-box">
+              <input type="text" name="" required="" />
+              <label>UserName</label>
+            </div>
+            <a href="#">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Add
+            </a>
+            <a href="#">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Upload CSV
+              <input
+                type="file"
+                className="inputFile"
+                accept=".csv"
+                style={{ pointer: "cursor" }}
+              ></input>
+            </a>
+          </form>
+        </div>
       </div>
     </>
   );
