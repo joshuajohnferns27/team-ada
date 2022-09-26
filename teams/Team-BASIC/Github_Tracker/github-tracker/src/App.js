18ae67b5ca1bf3446f5698fb146905c0ecdb0471
Aliyaj Ahemad <aliyajahemad@gmail.com>
2022-09-26 12:55:17 +0530

    feat : Added Deleteuser.js component

--- teams/Team-BASIC/Github_Tracker/github-tracker/src/App.js
+++ teams/Team-BASIC/Github_Tracker/github-tracker/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Adduser from "./components/Adduser";
 import Adminlogin from "./components/Adminlogin";
 import Adminsignup from "./components/Adminsignup";
+import Deleteuser from "./components/Deleteuser";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Adminlogin />} />
           <Route path="/signup" element={<Adminsignup />} />
           <Route path="/add" element={<Adduser />} />
+          <Route path="/delete" element={<Deleteuser />} />
         </Routes>
       </BrowserRouter>
     </>
