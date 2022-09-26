c68c6ce61c3e3bcaaaf022bf6da2e6fcbc216fbc
sidesai2001 <sidesai2001@gmail.com>
2022-09-27 00:32:49 +0530

    feat: dashboard added

--- teams/Team-BASIC/Github_Tracker/github-tracker/src/App.js
+++ teams/Team-BASIC/Github_Tracker/github-tracker/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
-import "./Admin.css";
+import "./components/Adminlogin.css"
+// import "./Admin.css";
 import Adminlogin from "./components/Adminlogin";
 import Adminsignup from "./components/Adminsignup";
-import DashBoard from "./components/DashBoard"
+// import DashBoard from "./components/DashBoard"
 import Deleteuser from "./components/Deleteuser";
 import Adduser from "./components/Adduser";
 
@@ -17,7 +18,7 @@ function App() {
           <Route path="/signup" element={<Adminsignup />} />
           <Route path="/add" element={<Adduser />} />
           <Route path="/delete" element={<Deleteuser />} />
-          <Route path="/dashboard" element={<DashBoard />} />
+          {/* <Route path="/dashboard" element={<DashBoard />} /> */}
         </Routes>
       </BrowserRouter>
     </>
---
+++ teams/Team-BASIC/Github_Tracker/github-tracker/src/components/DashBoard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+import "./dashboard.css"
+import { Link } from "react-router-dom";
+
+export default function Adminsignup() {
+  const [pwShown, setpwShown] = useState(0);
+
+  function menuOnClick () {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+    }
+  
+
+  return (
+    <>  
+        <div className="Auth-form-container">
+        <h1 className="heading">Github Tracker</h1>
+        <h2 className="sub-heading">Dashboard</h2>
+        <div id="menu">
+          <div id="menu-bar" onClick={menuOnClick}>
+            <div id="bar1" className="bar" />
+            <div id="bar2" className="bar" />
+            <div id="bar3" className="bar" />
+          </div>
+          <nav className="nav" id="nav">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Add User</a></li>
+              <li><a href="#">Delete User</a></li>
+              <li><Link style={{textDecoration: 'none',color: "white"}} className="link" to="/">Logout</Link></li>
+            </ul>
+          </nav> 
+        </div>
+        <div className="menu-bg" id="menu-bg" />
+        <div className="search">
+            <input
+                id="searchbar"
+                placeholder="Search"
+            />
+            <i
+                className="fa-light fa-magnifying-glass"
+                aria-hidden="true"
+                type="button"
+                id="searchicon"
+            ></i>
+        </div>
+      </div>
+    </>
+  );
+}
