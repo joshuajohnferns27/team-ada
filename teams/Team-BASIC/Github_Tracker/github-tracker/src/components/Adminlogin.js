7d8f32d55fb438e475243301b5419f283ce3b6b1
Aliyaj Ahemad <aliyajahemad@gmail.com>
2022-09-26 12:20:40 +0530

    fix:removed hooks from adminlogin.js and adminsignup.js

--- teams/Team-BASIC/Github_Tracker/github-tracker/src/components/Adminlogin.js
+++ teams/Team-BASIC/Github_Tracker/github-tracker/src/components/Adminlogin.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { useState } from "react";
+
 import { Link } from "react-router-dom";
 import "./Adminlogin.css";
 
 export default function Adminlogin() {
-  const [pwShown, setpwShown] = useState(0);
-
   function hoverOut() {
     var p = document.getElementById("pwd");
     p.setAttribute("type", "password");
