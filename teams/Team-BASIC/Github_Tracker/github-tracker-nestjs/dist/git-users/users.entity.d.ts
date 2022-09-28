4f29ab441daab8f839551de604a36a48c1ea5914
Athena Rodrigues <53125219+athena46@users.noreply.github.com>
2022-09-28 18:34:20 +0530

    Created History table and added Foreign key

--- teams/Team-BASIC/Github_Tracker/github-tracker-nestjs/dist/git-users/users.entity.d.ts
+++ teams/Team-BASIC/Github_Tracker/github-tracker-nestjs/dist/git-users/users.entity.d.ts
@@ -1,10 +1,18 @@
 export declare class SUsers {
-    id: number;
     email: string;
     password: string;
 }
 export declare class Users {
-    id: number;
     email: string;
     username: string;
+    firstname: string;
+    lastname: string;
+}
+export declare class GitHistory {
+    userId: string;
+    users: Users;
+    datetime: Date;
+    violations: number;
+    public_gist: string;
+    public_repo: number;
 }
